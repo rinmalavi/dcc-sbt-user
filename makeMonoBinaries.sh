9e74ccf45785d15225f0fb20f09daf8babf80693
Marin Vila <marin@element.hr>
2014-05-08 17:03:46 +0200

    Shorter mcs cmd.

--- makeMonoBinaries.sh
+++ makeMonoBinaries.sh
@@ -11,13 +11,10 @@ assembly_name="$3"
 
 dependencies="-r:System.ComponentModel.Composition -r:System -r:System.Data -r:System.Xml -r:System.Runtime.Serialization -r:System.Configuration -r:System.Drawing"
 while IFS='' read -d $'\0' -r path; do
-	dependencies="$dependencies -r:$path"
-done < <(find "$dependencies_folder" -iname '*.dll' -type f -print0)
+	dependencies="$dependencies -r:${path#./}"
+done < <(cd "$dependencies_folder" && find "." -iname '*.dll' -type f -print0)
 
-sources=""
-while IFS='' read -d $'\0' -r path; do
-	sources="$sources $path"
-done < <(find "$sources_folder" -iname '*.cs' -type f -print0)
+sources="-recurse:$sources_folder/*.cs"
 
-cmd="mcs -out:$assembly_name -target:library $dependencies $sources"
+cmd="mcs -out:$assembly_name -target:library -lib:$dependencies_folder $dependencies $sources"
 $cmd
